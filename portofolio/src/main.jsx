01bf76ac4f96323c2444032d189560269dfab4a5
ER RAGRAGUI HATIM <hatim.er@protonmail.com>
2023-08-01 00:45:06 +0200

    try 3 adding google analytics

--- portofolio/src/main.jsx
+++ portofolio/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReactGA from 'react-ga';
+
 
 //local import
 import App from './App'
 import './index.css'
 
-// Initialisez Google Analytics
-ReactGA.initialize('G-QKGXLHPFGK');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
